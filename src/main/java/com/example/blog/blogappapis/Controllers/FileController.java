309e5342a786b2c47a3ba96883dcec731bda98bd
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-08-21 17:49:19 +0545

    added file upload/download

--- src/main/java/com/example/blog/blogappapis/Controllers/FileController.java
+++ src/main/java/com/example/blog/blogappapis/Controllers/FileController.java
@@ -5,14 +5,16 @@ import com.example.blog.blogappapis.Services.Impl.FileServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/api/v1/file")
@@ -24,6 +26,7 @@ public class FileController {
     @Value(value = "${project.image}")
     private String path;
 
+    //upload file
     @PostMapping("/upload")
     ResponseEntity<FileResponse> fileUpload(@RequestParam("image")MultipartFile file) throws IOException{
         String fileName = null;
@@ -34,5 +37,13 @@ public class FileController {
             return new ResponseEntity<>(new FileResponse(null, "File couldn't upload due to error"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(new FileResponse(fileName, "File added successfully"), HttpStatus.OK);
+    }
+
+    //download file
+    @GetMapping(value = "/profile/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadFile(@PathVariable("imageName") String imageName, HttpServletResponse response) throws IOException {
+        InputStream inputStream = this.fileService.getResource(path,imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(inputStream, response.getOutputStream());
     }
 }
--- src/main/java/com/example/blog/blogappapis/Services/FileService.java
+++ src/main/java/com/example/blog/blogappapis/Services/FileService.java
@@ -3,9 +3,14 @@ package com.example.blog.blogappapis.Services;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 public interface FileService {
 
     public String uploadImage(String path, MultipartFile multipartFile) throws IOException;
+
+    InputStream getResource(String path, String filename) throws FileNotFoundException;
+
 }
